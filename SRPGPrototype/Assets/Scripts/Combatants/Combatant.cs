8707f16f1fa25dd17907440b7d779944613c2fde
Valentino Abate <valentinoabate1@gmail.com>
2020-07-12 19:07:57 -0700

    Repaired combatant script
    - Looks like I lost all of the combatant work as well

--- SRPGPrototype/Assets/Scripts/Combatants/Combatant.cs
+++ SRPGPrototype/Assets/Scripts/Combatants/Combatant.cs
@@ -4,12 +4,21 @@ using UnityEngine;
 
 public abstract class Combatant : GridObject
 {
+    public enum Team
+    { 
+        None,
+        Enemy,
+        Player,
+    }
+
+    public abstract Team CTeam { get; }
     public abstract int MaxHp { get; }
     public abstract int Hp { get; protected set; }
     public bool Dead => Hp <= 0;
     public abstract int MaxAP { get; }
     public abstract int AP { get; protected set; }
     public abstract string DisplayName { get; }
+    public abstract List<Action> Actions { get; } 
 
     public virtual void Damage(int damage)
     {
