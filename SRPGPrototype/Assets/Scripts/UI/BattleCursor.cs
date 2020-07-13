8d5706785df76dd27a573cb254921d78f26921c7
Valentino Abate <valentinoabate1@gmail.com>
2020-07-12 19:04:31 -0700

    Battle UI 1
    - Basic battle cursor code

--- SRPGPrototype/Assets/Scripts/UI/BattleCursor.cs
+++ SRPGPrototype/Assets/Scripts/UI/BattleCursor.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class BattleCursor : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public System.Action<Vector2Int> OnClick { get; set; } = null;
+    public System.Action OnCancel { get; set; } = null;
+    public BattleGrid grid;
+    private Vector3 previousMousePos = Vector3.zero;
+    private void Update()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if(Input.mousePosition != previousMousePos)
+        {
+            var mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            var mouseGridPos = grid.GetPos(mouseWorldPos);
+            if(grid.IsLegal(mouseGridPos))
+            {
+                transform.position = grid.GetSpace(mouseGridPos);
+            }
+        }
+        if(Input.GetMouseButtonDown(0) && OnClick != null)
+        {
+            var mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            var mouseGridPos = grid.GetPos(mouseWorldPos);
+            if (grid.IsLegal(mouseGridPos))
+            {
+                OnClick.Invoke(mouseGridPos);
+            }
+        }
+        else if(Input.GetMouseButtonDown(1) && OnCancel != null)
+        {
+            OnCancel.Invoke();
+        }
     }
 }
+
--- SRPGPrototype/Assets/Scripts/UI/BattleUI.cs
+++ SRPGPrototype/Assets/Scripts/UI/BattleUI.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class BattleUI : MonoBehaviour
 {
+    public BattleGrid grid;
+    public BattleCursor cursor;
     // Start is called before the first frame update
     void Start()
     {
