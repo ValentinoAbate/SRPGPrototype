4c4047f6b57b48f86de9b1620e0af8941e039372
Valentino Abate <valentinoabate1@gmail.com>
2020-10-18 23:43:27 -0700

    Removed unnecessary using statements

--- SRPGPrototype/Assets/Scripts/AI/AIComponent.cs
+++ SRPGPrototype/Assets/Scripts/AI/AIComponent.cs
@@ -1,8 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using System.Linq;
-using System.Net.WebSockets;
+using UnityEngine;
 
 public abstract class AIComponent<T> : MonoBehaviour where T : AIUnit
 {
