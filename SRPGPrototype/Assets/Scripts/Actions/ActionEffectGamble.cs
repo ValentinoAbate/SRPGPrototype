a5d316c4bced7822014f17d842f009741c77311b
Valentino Abate <valentinoabate1@gmail.com>
2020-12-20 23:11:50 -0800

    Additional Effects and Programs
    - Added a gamble effect that choose one or another effect
    - Added an effect that chooses

---
+++ SRPGPrototype/Assets/Scripts/Actions/ActionEffectGamble.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RandomUtils;
+
+public class ActionEffectGamble : ActionEffect
+{
+    [Range(0, 1)]
+    [SerializeField] private float successChance = 0.5f;
+    [SerializeField] private ActionEffect successEffect;
+    [SerializeField] private ActionEffect failureEffect;
+    public override void ApplyEffect(BattleGrid grid, Action action, SubAction sub, Unit user, Unit target, PositionData targetData)
+    {
+        if((float)RandomU.instance.RandomDouble() < successChance)
+        {
+            successEffect.ApplyEffect(grid, action, sub, user, target, targetData);
+        }
+        else if (failureEffect != null)
+        {
+            failureEffect.ApplyEffect(grid, action, sub, user, target, targetData);
+        }
+    }
+}
---
+++ SRPGPrototype/Assets/Scripts/Actions/ActionEffectHitRandomUnit.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class ActionEffectHitRandomUnit : ActionEffect
+{
+    [SerializeField] private ActionEffect effectToApply;
+
+    [SerializeField] private Unit.Team[] teams = new Unit.Team[] { Unit.Team.Enemy };
+
+    public override void ApplyEffect(BattleGrid grid, Action action, SubAction sub, Unit user, Unit target, PositionData targetData)
+    {
+        var targets = grid.FindAll((u) => teams.Contains(u.UnitTeam));
+        if (targets.Count <= 0)
+            return;
+        var newTarget = RandomUtils.RandomU.instance.Choice(targets);
+        var newTargetData = new PositionData() { targetPos = newTarget.Pos, selectedPos = targetData.selectedPos };
+        effectToApply.ApplyEffect(grid, action, sub, user, newTarget, newTargetData);
+    }
+}
