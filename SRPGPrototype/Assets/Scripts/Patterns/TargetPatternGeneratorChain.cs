de0fef40f55690dabaad6b1c5218f42e26545eb0
Valentino Abate <valentinoabate1@gmail.com>
2020-12-21 16:59:05 -0800

    Chain Lightning and Plague upgrades

--- SRPGPrototype/Assets/Scripts/Patterns/TargetPatternGeneratorChain.cs
+++ SRPGPrototype/Assets/Scripts/Patterns/TargetPatternGeneratorChain.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class TargetPatternGeneratorChain : TargetPatternGenerator
 {
@@ -9,6 +10,8 @@ public class TargetPatternGeneratorChain : TargetPatternGenerator
         Horizontal,
         Diagonal,
         Both,
+        HorizontalRange2,
+        DiagonalRange2,
     }
 
     public Adjacency adjacency = Adjacency.Diagonal;
@@ -31,6 +34,22 @@ public class TargetPatternGeneratorChain : TargetPatternGenerator
         else if(adjacency == Adjacency.Both)
         {
             GetChainTargets(targetPos, grid, Vector2IntExtensions.AdjacentBoth, ref targets);
+        }
+        else if(adjacency == Adjacency.HorizontalRange2)
+        {
+            IEnumerable<Vector2Int> Adj(Vector2Int pos)
+            {
+                return pos.Adjacent().Concat(pos.Adjacent(1));
+            }
+            GetChainTargets(targetPos, grid, Adj, ref targets);
+        }
+        else if(adjacency == Adjacency.HorizontalRange2)
+        {
+            IEnumerable<Vector2Int> Adj(Vector2Int pos)
+            {
+                return pos.AdjacentDiagonal().Concat(pos.AdjacentDiagonal(1));
+            }
+            GetChainTargets(targetPos, grid, Adj, ref targets);
         }
         // Return targets
         return targets;
