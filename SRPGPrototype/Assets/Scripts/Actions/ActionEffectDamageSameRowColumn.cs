b5b6b24989d13c447c1c7e207431dc93d002c182
Valentino Abate <valentinoabate1@gmail.com>
2020-12-31 15:19:38 -0800

    Shell Revamp Progress
    v0.5.1.4
    - Revamped Stripe H/V shells, Capsule shell, and Y shell
    - Added a damage effect that does different damage values depending on if  something is in the same row/column as you (or either)

---
+++ SRPGPrototype/Assets/Scripts/Actions/ActionEffectDamageSameRowColumn.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionEffectDamageSameRowColumn : ActionEffectDamage
+{
+    public enum Option
+    { 
+        Row,
+        Column,
+        Either,
+    }
+
+    [SerializeField] private Option option;
+    [SerializeField] private int sameNumber;
+    [SerializeField] private int differentNumber;
+
+    public override int BaseDamage(BattleGrid grid, Action action, Unit user, List<Vector2Int> targetPositions)
+    {
+        return 0;
+    }
+
+    public override int TargetModifier(BattleGrid grid, Action action, Unit user, Unit target, PositionData targetData)
+    {
+        return option switch
+        {
+            Option.Row => user.Pos.y == target.Pos.y ? sameNumber : differentNumber,
+            Option.Column => user.Pos.x == target.Pos.x ? sameNumber : differentNumber,
+            Option.Either => user.Pos.y == target.Pos.y || user.Pos.x == target.Pos.x ? sameNumber : differentNumber,
+            _ => throw new System.NotImplementedException("Invalid Option"),
+        };
+    }
+}
