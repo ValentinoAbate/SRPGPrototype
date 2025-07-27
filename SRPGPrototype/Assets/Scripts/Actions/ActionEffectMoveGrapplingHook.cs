3c6b61a2069af2bad3c45e02529a263015551f8d
Valentino Abate <valentinoabate1@gmail.com>
2025-07-26 18:47:54 -0700

    Add tractor beam and grappling hook

---
+++ SRPGPrototype/Assets/Scripts/Actions/ActionEffectMoveGrapplingHook.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionEffectMoveGrapplingHook : ActionEffectMove
+{
+    public override void ApplyEffect(BattleGrid grid, Action action, SubAction sub, Unit user, Unit target, PositionData targetData)
+    {
+        if (target == null)
+            return;
+        var direction = target.Pos.DirectionTo(targetData.selectedPos);
+        var goal = targetData.selectedPos - direction;
+        while(user.Pos != goal)
+        {
+            if(!Move(grid, user, direction))
+                break;
+        }
+    }
+}
---
+++ SRPGPrototype/Assets/Scripts/Actions/ActionEffectMoveTractorBeam.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionEffectMoveTractorBeam : ActionEffectMove
+{
+    public override void ApplyEffect(BattleGrid grid, Action action, SubAction sub, Unit user, Unit target, PositionData targetData)
+    {
+        if (target == null)
+            return;
+        var direction = target.Pos.DirectionTo(user.Pos);
+        var goal = user.Pos - direction;
+        while (target.Pos != goal)
+        {
+            if (!Move(grid, target, direction))
+                break;
+        }
+    }
+}
