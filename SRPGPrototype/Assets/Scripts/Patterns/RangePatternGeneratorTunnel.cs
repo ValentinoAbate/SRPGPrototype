452da0692c8e9b1f64f6b70282b22358c18374f9
Valentino Abate <valentinoabate1@gmail.com>
2025-08-06 18:33:00 -0700

    Tunnel Shark Enemy

---
+++ SRPGPrototype/Assets/Scripts/Patterns/RangePatternGeneratorTunnel.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class RangePatternGeneratorTunnel : RangePatternGenerator
+{
+    [SerializeField] private AdjacencyDirections directions;
+    [SerializeField] private bool allowAdjacentPositions = true;
+    public override IEnumerable<Vector2Int> Generate(BattleGrid grid, Vector2Int userPos, Unit user)
+    {
+        foreach(var direction in directions.GetDirectionVectors())
+        {
+            var pos = grid.FirstEmptyPositionInDirection(userPos, direction);
+            if (pos != BattleGrid.OutOfBounds && (allowAdjacentPositions || pos != userPos + direction))
+                yield return pos;
+        }
+    }
+
+    public override IEnumerable<Vector2Int> ReverseGenerate(BattleGrid grid, Vector2Int targetPos, Unit user)
+    {
+        foreach (var direction in directions.GetDirectionVectors())
+        {
+            var pos = grid.FirstEmptyPositionInDirection(targetPos, direction);
+            if (pos != BattleGrid.OutOfBounds && (allowAdjacentPositions || pos != targetPos + direction))
+                yield return pos;
+        }
+    }
+
+    public override int MaxDistance(BattleGrid grid)
+    {
+        if(directions.HasFlag(AdjacencyDirections.Diagonal))
+            return grid.MaxGridDistance;
+        if (directions.HasFlag(AdjacencyDirections.HorizontalVertical))
+            return Mathf.Max(grid.Dimensions.x, grid.Dimensions.y) - 1;
+        return 0;
+    }
+}
