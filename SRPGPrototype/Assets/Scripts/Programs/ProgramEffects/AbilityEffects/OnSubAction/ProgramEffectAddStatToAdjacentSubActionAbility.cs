146cac7bdc7c377e79cc0f75d52bff84d0b3fcf9
Valentino Abate <valentinoabate1@gmail.com>
2025-08-18 22:46:40 -0700

    Fix filenames for Sub-action abilities

--- SRPGPrototype/Assets/Scripts/Programs/ProgramEffects/AbilityEffects/OnSubAction/ProgramEffectAddStatToAdjacentSubActionAbility.cs
+++ SRPGPrototype/Assets/Scripts/Programs/ProgramEffects/AbilityEffects/OnSubAction/ProgramEffectAddStatToAdjacentSubActionAbility.cs
@@ -3,19 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-public class ProgramEffectAddStatToTargetSubActionAbility : ProgramEffectAddSubActionAbility
+public class ProgramEffectAddStatToAdjacentSubActionAbility : ProgramEffectAddSubActionAbility
 {
     public Stats.StatName stat;
     public ActionNumber number;
     protected override string AbilityName => abilityName;
     [SerializeField] private string abilityName = string.Empty;
-
     [SerializeField] private Unit.Team[] affectedTeams = new Unit.Team[] { Unit.Team.Enemy };
+
     public override void OnSubAction(BattleGrid grid, Action action, SubAction subAction, Unit user, List<Unit> targets, List<Vector2Int> targetPositions)
     {
-        foreach(var target in targets)
+        foreach(var pos in user.Pos.Adjacent())
         {
-            if (!affectedTeams.Contains(target.UnitTeam))
+            if (!grid.IsLegal(pos))
+                continue;
+            var target = grid.Get(pos);
+            if (target == null || !affectedTeams.Contains(target.UnitTeam))
                 continue;
             int value = number.ActionValue(grid, action, target, targets.Count);
             target.ModifyStat(grid, stat, value, user);
