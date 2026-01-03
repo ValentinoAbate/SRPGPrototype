3079649f2c88a8fa98a67d5fcb271ec6e21f5e58
Valentino Abate <valentinoabate1@gmail.com>
2026-01-03 12:27:05 -0800

    Rename stat compare target pattern gen
    - for clarity

--- SRPGPrototype/Assets/Scripts/Patterns/TargetPatternGeneratorAllUnitsWithStat.cs
+++ SRPGPrototype/Assets/Scripts/Patterns/TargetPatternGeneratorAllUnitsWithStat.cs
@@ -1,12 +1,12 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 
-public class TargetPatternGeneratorStatCompare : TargetPatternGenerator
+public class TargetPatternGeneratorAllUnitsWithStat : TargetPatternGenerator
 {
-    public Stats.StatName stat;
-    public Stats.StatName userStat = Stats.StatName.None;
-    public int constant = 1;
-    public ComparisonOperator comparison;
+    [SerializeField] private Stats.StatName stat;
+    [SerializeField] private Stats.StatName userStat = Stats.StatName.None;
+    [SerializeField] private int constant = 1;
+    [SerializeField] private ComparisonOperator comparison;
 
     public override IEnumerable<Vector2Int> Generate(BattleGrid grid, Unit user, Vector2Int targetPos)
     {
@@ -14,6 +14,7 @@ public class TargetPatternGeneratorStatCompare : TargetPatternGenerator
         int threshold = userStat == Stats.StatName.None ? constant : user.GetStat(userStat);
         foreach(var unit in grid)
         {
+            // Check if the target's stat meets the threshold
             if(comparison.Evaluate(threshold, unit.GetStat(stat)))
             {
                 yield return unit.Pos;
