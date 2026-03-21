ca8d8b7e6a9493ad91b8f31ef243c8135db75ca0
Valentino Abate <valentinoabate1@gmail.com>
2026-03-20 19:25:58 -0700

    Improve Charge Actions
    - scale based on distance to target increases distanced moved when extended)

--- SRPGPrototype/Assets/Scripts/Actions/ActionEffectMoveUserTowardsTarget.cs
+++ SRPGPrototype/Assets/Scripts/Actions/ActionEffectMoveUserTowardsTarget.cs
@@ -4,17 +4,24 @@ using UnityEngine;
 
 public class ActionEffectMoveUserTowardsTarget : ActionEffectMove
 {
+    [SerializeField] private bool scale = false;
+
     public override void ApplyEffect(BattleGrid grid, Action action, SubAction sub, Unit user, Unit target, PositionData targetData)
     {
         if (target == null || !user.Movable)
             return;
-        Move(grid, user, user, user.Pos.DirectionTo(target.Pos));
+        Move(grid, user, user, user.Pos.DirectionTo(target.Pos), GetMoveDistance(user, target));
     }
 
     protected override bool IsValidTargetInternal(BattleGrid grid, Action action, SubAction sub, Unit user, Unit target, PositionData targetData)
     {
         if (target == null || !user.Movable)
             return false;
-        return CanMove(grid, user, user, user.Pos.DirectionTo(target.Pos));
+        return CanMove(grid, user, user, user.Pos.DirectionTo(target.Pos), GetMoveDistance(user, target));
+    }
+
+    protected int GetMoveDistance(Unit user, Unit target)
+    {
+        return scale ? user.Pos.ChebyshevDistance(target.Pos) : 1;
     }
 }
