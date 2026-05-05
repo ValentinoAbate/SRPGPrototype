10775659475704a36a69712954a439aba91b3e0a
Valentino Abate <valentinoabate1@gmail.com>
2026-05-04 23:22:16 -0700

    Re-add duplicate prevention to boss capacity data

---
+++ SRPGPrototype/Assets/Scripts/Loot/DropComponentProgramCapacityNoDupes.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+public class DropComponentProgramCapacityNoDupes : DropComponent<Program>
+{
+    public LootProvider.LootQuality[] lootQualities = new LootProvider.LootQuality[3];
+    protected override List<Program> GenerateDrop(Loot<Program> loot)
+    {
+        return loot.GetDropsStandardNoDuplicates(lootQualities, ProgramFilters.GivesCapacity);
+    }
+}
